fae0b2130fa9ceb000249cb1727a4143326b10f1
nobdey92 <jordan_hobdey@hotmail.co.uk>
2023-05-23 20:14:44 +0100

    added a basic map. Current location and a JSON for the location typed in

---
+++ BasicDemoMap/maps.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 52.572611, lng: -0.239972 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 12,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Home",
+  });
+}
+
+initMap();
